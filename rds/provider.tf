fb81ec53339472c79ed911d4d102959711fb09cd
Björn Weinbrenner <info@bjoerne.com>
2018-09-11 14:36:08 +0200

    Add opentelekomcloud provider

--- rds/provider.tf
+++ rds/provider.tf
@@ -5,3 +5,12 @@ provider "openstack" {
   domain_name = "${var.domain_name}"
   auth_url    = "${var.endpoint}"
 }
+
+provider "opentelekomcloud" {
+  user_name   = "${var.username}"
+  password    = "${var.password}"
+  tenant_name = "${var.tenant_name}"
+  domain_name = "${var.domain_name}"                                                                       
+  auth_url    = "${var.endpoint}"
+  region      = "${var.region}"
+}
